b52075276d7d628f149d0f8e779e5d46b478baf3
lamontu <lamontyu@163.com>
2017-11-16 14:26:20 +0800

    Add a test example

--- listing_4.12.cpp
+++ listing_4.12.cpp
@@ -1,3 +1,7 @@
+#include <list>
+#include <algorithm>
+#include <iostream>
+
 template<typename T>
 std::list<T> sequential_quick_sort(std::list<T> input)
 {
@@ -18,7 +22,26 @@ std::list<T> sequential_quick_sort(std::list<T> input)
     auto new_higher(
         sequential_quick_sort(std::move(input)));
     result.splice(result.end(),new_higher);
-    Using synchronization of operations to simplify code
+    //Using synchronization of operations to simplify code
         result.splice(result.begin(),new_lower);
     return result;
+}
+
+int main()
+{
+    std::cout << "==== orginal list====\n";
+    std::list<int> lst{3,4,5,8,7,9,2,0,1,6};
+    for (std::list<int>::iterator itr = lst.begin(); itr != lst.end(); ++itr) {
+        std::cout << *itr << ',';
+    }
+    std::cout << std::endl;
+
+    std::list<int> sorted_lst = sequential_quick_sort(lst);
+    std::cout << "==== sorted list====\n";
+    for (std::list<int>::iterator itr = sorted_lst.begin(); itr != sorted_lst.end(); ++itr) {
+        std::cout << *itr << ',';
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
