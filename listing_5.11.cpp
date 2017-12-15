e6b471dbd330f9d963440bc01c2ae9b4a0c25b5b
lamontu <lamontyu@163.com>
2017-12-15 18:41:23 +0800

    Add parameter in threads

--- listing_5.11.cpp
+++ listing_5.11.cpp
@@ -1,12 +1,14 @@
 #include <vector>
 #include <atomic>
 #include <thread>
+#include <iostream>
 
 std::vector<int> queue_data;
 std::atomic<int> count;
 
 void populate_queue()
 {
+    std::cout << "populate_queue()==> \n";
     unsigned const number_of_items=20;
     queue_data.clear();
     for(unsigned i=0;i<number_of_items;++i)
@@ -15,35 +17,39 @@ void populate_queue()
     }
     
     count.store(number_of_items,std::memory_order_release);
+    std::cout << "populate_queue()<== \n";
 }
 
 void wait_for_more_items()
 {
 }
 
-void process(int)
+void process(int id, int input)
 {
+    std::cout << "process " << id << " input: " << input << " \n";
 }
 
-void consume_queue_items()
+void consume_queue_items(int id)
 {
+    std::cout << "consume_queue_items(" << id << ")==> \n ";
     while(true)
     {
         int item_index;
         if((item_index=count.fetch_sub(1,std::memory_order_acquire))<=0)
         {
-            wait_for_more_items();
+            // wait_for_more_items();
             continue;
         }
-        process(queue_data[item_index-1]);
+        process(id, queue_data.at(item_index-1));
     }
+    std::cout << "consume_queue_items(" << id << ")<== \n ";
 }
 
 int main()
 {
     std::thread a(populate_queue);
-    std::thread b(consume_queue_items);
-    std::thread c(consume_queue_items);
+    std::thread b(consume_queue_items, 46);
+    std::thread c(consume_queue_items, 47);
     a.join();
     b.join();
     c.join();
